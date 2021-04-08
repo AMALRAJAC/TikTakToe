5e8740153b9381a425658948373a838c5fc0a06c
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-09 01:11:48 +0530

    Update TikTakToe.java

--- TikTakToe.java
+++ TikTakToe.java
@@ -1,64 +1,43 @@
 
+
 import java.util.Scanner;
 public class TikTakToe {
+	public static String userVal=null;
+	public static String computerVal=null;
 	static String[] board = new String[10];
-	 static int position=0;
-	 static int positionc=0;
+	 static int user_position=0;
+	 static int computer_position=0;
 	 static void printBoard(String[] board)
 	    {
 	        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
 	        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
 	        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
 	    }
+	 
 	  static void input() {
-		  Scanner sc=new Scanner(System.in);
-		  String user="x";
-		  
-		  System.out.println("enter the position of user");
-		  position=sc.nextInt();
-		  System.out.println("enter the position of computer");
-		  positionc=sc.nextInt();
-		  
-		  if(position!=positionc) {
+		  for(int i=1;user_position<10&&computer_position<10;i++) {
 			  
-				  if((("o".equals(board[position]))||("x".equals(board[position])))) {
-					  System.out.println("given position already filled ....!! play again");
-					  System.out.println("enter the position of user");
-					  position=sc.nextInt();
-					  
-				  }
-			 
-				  else if(("o".equals(board[positionc]))||("x".equals(board[positionc]))){
-					  System.out.println("given position already filled ....!! play again");
-					  System.out.println("enter the position of computer");
-					  positionc=sc.nextInt();
-					  
-				  }
-			  
-			  board[position]="x";
-			  board[positionc]="o";
+			  Scanner sc= new Scanner(System.in);
+			  System.out.println("computer input");
+			  computer_position=sc.nextInt();
+			  System.out.println("user input");
+			  user_position=sc.nextInt();
+			  board[user_position]=userVal;
+			  board[computer_position]=computerVal;
 			  printBoard(board);
-			  
-		  
-		  
 		  }
-		  else {
-			  System.out.println("players cant choose same position");
-		  }
-		  }
-	  
-	  public static void showBoard() {
 		  
-		  for (int a = 1; a < 10; a++) {
-	            board[a] =String.valueOf(a);
-	        }
-		  printBoard(board);
 	  }
 	  public static void main(String[]args) {
-		  showBoard();
-		  for(int i=0;i<10;i++) {
-		  input();
-		  }
+		  
+			 userVal="x";
+			 computerVal="o";
+		
+			 printBoard(board);
+		  for(int i=0;;i++) {
+			  input();
+			  }
+		
 	  }
 }
 
